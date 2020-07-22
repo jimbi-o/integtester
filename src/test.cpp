dc6120c32f7a966da043557b9b001c1b69be10e5
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2020-07-23 07:53:59 +0900

    foonathan memory library added

--- src/test.cpp
+++ src/test.cpp
@@ -1,4 +1,15 @@
+#include <set>
+#include <foonathan/memory/container.hpp>
+#include <foonathan/memory/memory_pool.hpp>
+#include <foonathan/memory/namespace_alias.hpp>
+#include <foonathan/memory/static_allocator.hpp>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
 TEST_CASE("null test") {
+  memory::static_allocator_storage<4096u> storage;
+  using static_pool = memory::memory_pool<memory::node_pool, memory::static_allocator>;
+  static_pool pool(memory::set_node_size<int>::value, 4096, storage);
+  memory::set<int, decltype(pool)> set(pool);
+  set.insert(3);
+  CHECK(set.find(3) != set.end());
 }
